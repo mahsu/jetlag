d507b1aff4e142c223ca2d5050ee4b5768defaee
Matthew Hsu <gears88@gmail.com>
2019-03-23 22:43:20 -0400

    start on document ready

--- routes.js
+++ routes.js
@@ -1,21 +1,38 @@
-const calPage = new CalPageOberver();
-const calSettings = new CalSettingsObserver();
-
-const onNavigate = (newUrl) => {
-    calPage.stop();
-    calSettings.stop();
-    if (newUrl.includes("/r/settings")) {
-        calSettings.start();
-    }
-    else if (newUrl.includes("/r")) {
-        calPage.start();
+function ready(fn) {
+    if (document.readyState != 'loading') {
+        fn();
+    } else if (document.addEventListener) {
+        document.addEventListener('DOMContentLoaded', fn);
+    } else {
+        document.attachEvent('onreadystatechange', function () {
+            if (document.readyState != 'loading')
+                fn();
+        });
     }
 }
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === 'url-change') {
-        onNavigate(request.url);
-    }
-});
+function init() {
+    const calPage = new CalPageOberver();
+    const calSettings = new CalSettingsObserver();
 
-onNavigate(window.location.href);
+    const onNavigate = (newUrl) => {
+        calPage.stop();
+        calSettings.stop();
+        if (newUrl.includes("/r/settings")) {
+            calSettings.start();
+        }
+        else if (newUrl.includes("/r")) {
+            calPage.start();
+        }
+    }
+
+    chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+        if (request.message === 'url-change') {
+            onNavigate(request.url);
+        }
+    });
+
+    onNavigate(window.location.href);
+}
+
+ready(init);
